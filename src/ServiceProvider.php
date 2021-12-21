8644de192322a05053dbece5ade4393b3ff200c9
Guy-roland ASSALE <rolandassale@gmail.com>
2021-12-21 08:48:18 +0000

    add task to stop apache service in nginx task

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -11,35 +11,10 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 
     public function boot()
     {
-        // $this->publishes([
-        //     static::packagePath("config/" . self::PACKAGE_NAME . ".php") => config_path(self::PACKAGE_NAME . '.php'),
-        // ], self::PACKAGE_NAME . '-config');
-
-        // $this->publishes([
-        //     static::packagePath('resources/lang') => resource_path('lang/vendor/' . static::PACKAGE_NAME),
-        // ], static::PACKAGE_NAME . '-lang');
-
-        // $this->loadRoutesFrom(static::packagePath("/routes/back.php"));
-        // $this->loadRoutesFrom(static::packagePath("/routes/front.php"));
-
-        // $this->loadTranslationsFrom(static::packagePath('resources/lang'), static::PACKAGE_NAME);
-
-        // $this->loadMigrationsFrom([
-        //     config(self::PACKAGE_NAME . '.migrations_path'),
-        // ]);
-
-        // $this->loadViewsFrom(static::packagePath('/resources/views/front'), self::PACKAGE_NAME);
-        // $this->loadViewsFrom(static::packagePath('/resources/views/back/' . Str::lower(config('administrable.theme'))), self::PACKAGE_NAME);
-        // $this->loadViewsFrom($this->packagePath('/resources/views/emails'), self::PACKAGE_NAME);
     }
 
     public function register()
     {
-        // $this->mergeConfigFrom(
-        //     static::packagePath('config/' . self::PACKAGE_NAME . '.php'),
-        //     self::PACKAGE_NAME
-        // );
-
         if ($this->app->runningInConsole()) {
             $this->commands([
                 DeployCommand::class,
